840793bebdc2be78254ab32acb7b345c9d1e134d
James Howard <james@outoftolerance.com>
2018-04-02 22:09:00 -0700

    Added serial message parsing

--- robocar_firmware.ino
+++ robocar_firmware.ino
@@ -10,18 +10,32 @@ const int throttle_in_pin = 3;
 const int steering_out_pin = 4;
 const int throttle_out_pin = 5;
 
-//Set serial baud rate
-const int serial_baud = 57600;
+//Serial settings
+const int serial_buffer_length = 128;
+const int serial_baud = 9600;
 
 //Global variables
-int mode = 0;
-int throttle_pwm = 0;
-int steering_pwm = 0;
-int throttle_trim_pwm = 0;
-int steering_trim_pwm = 0;
-const int center_pwm = 1500;
-const int zero_pwm = 1000;
+int mode = 0; //0 = manual, 1 = guided, 
+int throttle_millis = 0;
+int steering_millis = 0;
+int throttle_trim_millis = 0;
+int steering_trim_millis = 0;
 
+//Default PWM values for different states
+const int max_millis = 2000;
+const int center_millis = 1500;
+const int zero_millis = 1000;
+
+//Serial protocol defines
+#define MSG_START_CHAR = '$';
+#define MSG_END_CHAR = '\n';
+#define MSG_TYPE_SET_MODE = 'm';
+#define MSG_TYPE_SET_STEERTING = 's';
+#define MSG_TYPE_SET_TROTTLE = 't';
+#define MSG_TYPE_TRIM_STEETING = 'd';
+#define MSG_TYPE_TRIM_THROTTLE = 'y';
+
+//Setup and init objects
 void setup() 
 {
   //Begin serial interface
@@ -38,61 +52,89 @@ void setup()
   steeting.attach(steering_out_pin);
 }
 
+//Main program loop
 void loop() 
 {
   //Get serial input
   while(serial.available())
   {
-    char c = Serial.read();
+    char c = serial.read();
 
-    if(c == 1)
+    //Detect start of message
+    if(c == '$')
     {
-      
+      //Get message type
+      c = serial.read();
+
+      //Switch to correct action
+      switch(c)
+      {
+        case MSG_TYPE_SET_MODE:
+          mode = serial.parseInt();
+          break;
+
+        case MSG_TYPE_SET_STEERTING:
+          steering_millis = serial.parseInt();
+          break;
+
+        case MSG_TYPE_SET_TROTTLE:
+          throttle_millis = serial.parseInt();
+          break;
+
+        case MSG_TYPE_TRIM_STEERING:
+          steering_trim_millis += serial.parseInt();
+          break;
+
+        case MSG_TYPE_TRIM_TROTTLE:
+          throttle_trim_millis += serial.parseInt();
+          break;
+      }
     }
   }
   
-  //Do correct mode stuff
+  //Switch based on modes
   switch(mode)
   {
     //Manual mode
     case 0:
       //Get the input PWM
-      throttle_pwm = pulseIn(throttle_in_pin, HIGH);
-      steering_pwm = pulseIn(steering_in_pin, HIGH);
+      throttle_millis = pulseIn(throttle_in_pin, HIGH);
+      steering_millis = pulseIn(steering_in_pin, HIGH);
 
-      //Check if no input
-      if(throttle_pwm = 0)
+      //Validate Throttle input
+      if(throttle_millis == 0)
       {
-        throttle_pwm = 1000 + throttle_trim_pwm;
+        throttle_millis = 1000 + throttle_trim_millis;
       }
       else
       {
-        throttle_pwm += throttle_trim;
+        throttle_millis += throttle_trim;
       }
       
-      if(steering_pwm = 0)
+      //Validate Steering input
+      if(steering_millis == 0)
       {
-        steering_pwm = 1500 + steering_trim_pwm;
+        steering_millis = 1500 + steering_trim_millis;
       }
       else
       {
-        steering_pwm += steering_trim;
+        steering_millis += steering_trim;
       }
       
-      //Set outputs
-      throttle.writeMicroseconds(throttle_pwm);
-      steering.writeMicroseconds(steering_pwm);
+      throttle.writeMicroseconds(throttle_millis);
+      steering.writeMicroseconds(steering_millis);
+      break;
 
     //Guided Mode
     case 1:
-      //Set outputs
-      throttle.writeMicroseconds(throttle_pwm);
-      steering.writeMicroseconds(steering_pwm);
+      throttle.writeMicroseconds(throttle_millis);
+      steering.writeMicroseconds(steering_millis);
+      break;
 
     //Default (do nothing)
     default:
-      //Set outputs
-      throttle.writeMicroseconds(zero_pwm);
-      steering.writeMicroseconds(center_pwm);      
+      throttle.writeMicroseconds(zero_millis);
+      steering.writeMicroseconds(center_millis);
+      break;
   }
 }
